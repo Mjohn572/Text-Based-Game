c17a6082fa6bec271b00433e4937fa4314c07f9a
Mitchell Johnson <122828426+Mjohn572@users.noreply.github.com>
2024-07-29 19:50:45 +0000

    modified:   src/main.java 	modified:   src/menu.java

--- src/main.java
+++ src/main.java
@@ -1,4 +1,7 @@
 public class main {
 
-  
+    public static void main(String[] args) {
+        menu menu = new menu();
+        menu.printing();
+    }
 }
--- src/menu.java
+++ src/menu.java
@@ -1,3 +1,19 @@
 public class menu {
   
+    public void printing() {
+        int i = 0;
+        int a = 0;
+        while (a < 16) {
+            i = 0;
+            while (i < 91) {
+                i++;
+                if (!(a < 7 && a > 4 && i > 30 && i < 70)) {
+                    System.out.print("<3");
+                }
+            }
+            System.out.print(" ");
+            a++;
+            }
+        System.out.println();
+    }
 }
